b55dc4ccd675926d72614a6c9528641ae9f7bfdf
benweizhu <benweizhu@gmail.com>
2017-07-16 12:52:04 +0800

    change class name add lg and remove register link

--- src/assets/toolkit/scripts/components/mobile-search.js
+++ src/assets/toolkit/scripts/components/mobile-search.js
@@ -1,9 +1,9 @@
 module.exports = function() {
   $(".lg-navbar-search").on("click", function() {
-    $("#mobile-search-form").addClass("mobile-search");
+    $("#mobile-search-form").addClass("lg-mobile-search");
   });
 
-  $(".mobile-search-close-icon").on("click", function() {
-    $("#mobile-search-form").removeClass("mobile-search");
+  $(".lg-mobile-search-close-icon").on("click", function() {
+    $("#mobile-search-form").removeClass("lg-mobile-search");
   });
 };
